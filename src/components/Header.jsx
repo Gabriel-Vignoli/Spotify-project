aab711ddc0a41ec12ef549d186cb78a1b13dad10
Gabriel-Vignoli <pessoalvignoli123@gmail.com>
2025-02-16 16:34:18 -0300

    feat: add Main component structure

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
     <div className="flex justify-between items-center py-2 px-5">
       <img src={logoSpotify} alt="Spotity Logo" className="transition-all transform hover:scale-110" />
       <a href="/">
-        <h1 className="no-underline text-[20px] hover:text-green-300 transition-all transform hover:scale-110">
+        <h1 className="no-underline font-semibold text-2xl text-green-400 hover:text-green-500 transition-all transform hover:scale-110">
           Spotify
         </h1>
       </a>
---
+++ src/components/Main.jsx
@@ -0,0 +1,13 @@
+const Main = () => {
+    return (
+      <div className="mt-0 mx-[10px] mb-[10px] p-5 rounded-2xl min-h-screen bg-linear-to-b from-[#062d14] to-black flex flex-col">
+        <div className="flex items-center justify-between">
+          <h2 className="text-2xl font-bold">Popular Artists</h2>
+          <a href="/" className="hover:underline">Show all</a>
+        </div>
+      </div>
+    );
+  };
+  
+  export default Main;
+  
